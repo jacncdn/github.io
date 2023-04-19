4e8412c9bab4771b8d378b178e9889ddb761c3b7
JackAndChat <JackAndChat@gmail.com>
2023-04-19 11:43:32 -0500

    Update JS_Editor.js

--- www/eh/JS_Editor.js
+++ www/eh/JS_Editor.js
@@ -9,7 +9,6 @@ var Room_ID = 'eh';
 
 var ALLOW_GUESTS = true;
 var LOAD_BOT = true;
-var BETA_USERS = [];
 
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
