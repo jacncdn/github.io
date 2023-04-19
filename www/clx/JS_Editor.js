6329983621ede2c2e81a377a51d7ac7821372044
JackAndChat <JackAndChat@gmail.com>
2023-04-19 11:43:28 -0500

    Update JS_Editor.js

--- www/clx/JS_Editor.js
+++ www/clx/JS_Editor.js
@@ -8,6 +8,9 @@ var ChannelName_Caption = 'Classics';
 var Room_ID = 'clx';
 
 var LOAD_BOT = true;
+var AGE_RESTRICT = true;
+
+var MOTD_MSG = `<br /><span style="color:orange">Here is a list of other rooms <a style="color:orange;font-weight:600" target=_blank" href="https://s.lain.la/xZP2R">https://s.lain.la/xZP2R</a></span>`;
 
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
