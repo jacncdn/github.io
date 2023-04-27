2b8f5683c19ddec9c50db9389c70665a230a44a1
jacncdn <126722094+jacncdn@users.noreply.github.com>
2023-04-27 12:11:26 -0500

    NiceCocks

---
+++ www/nc/JS_Editor.js
@@ -0,0 +1,13 @@
+/*!
+**|  CyTube Customizations
+**|   
+**@preserve
+*/
+
+var ChannelName_Caption = 'Nice Cocks';
+var Room_ID = 'nc';
+
+// ##################################################################################################################################
+if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
+
+$.getScript("https://jacncdn.github.io/www/loader.js");
