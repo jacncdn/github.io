861664fa369d53cb6e5261f402fb6114a33f804e
jacncdn <126722094+jacncdn@users.noreply.github.com>
2023-04-27 12:30:16 -0500

    Update JS_Editor.js

--- www/nc/JS_Editor.js
+++ www/nc/JS_Editor.js
@@ -7,13 +7,8 @@
 var ChannelName_Caption = 'Nice Cocks';
 var Room_ID = 'nc';
 
-var ALLOW_GUESTS = true;
 var AGE_RESTRICT = true;
-var TRIVIA = false;
-var LOAD_BOT = true;
-var BOT_NICK = "Karen311";
-var ROOM_ANNOUNCEMENT = ``;
-var MOD_ANNOUNCEMENT = "";
+
 var MOTD_MSG = `<br /><span style="color:orange">Here is a list of other rooms <a style="color:orange;font-weight:600" target=_blank" href="https://s.lain.la/xZP2R">https://s.lain.la/xZP2R</a></span>`;
 
 // ##################################################################################################################################
