53cc14926831e3281961f0bcb31793546d03650d
jacncdn <admin@jackandchat.net>
2023-05-19 14:53:37 -0500

    Update defaults.js

--- www/defaults.js
+++ www/defaults.js
@@ -3,13 +3,21 @@
 **|
 **@preserve
 */
-'use strict';
+/* jshint esversion:6 */
+/* jshint strict:true */
+/* jshint curly:true */
+/* jshint eqeqeq:true */
+/* jshint varstmt:true */
+
+/* jshint undef:false */
+/* globals $, socket, debugData */
 
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
 
 // ##################################################################################################################################
 
 const getOptions = function() {
+  'use strict'; 
   $.getJSON(Options_URL, function(data) {
       logTrace('defaults.getOptions', data);
       socket.emit("setOptions", data);
@@ -17,11 +25,12 @@ const getOptions = function() {
     .fail(function(data) {
       errorData('defaults.getOptions Error', data.status + ": " + data.statusText);
     });
-}
+};
 
 // ##################################################################################################################################
 
 const getPermissions = function() {
+  'use strict'; 
   $.getJSON(Permissions_URL, function(data) {
       logTrace('defaults.getPermissions', data);
       socket.emit("setPermissions", data);
@@ -29,11 +38,12 @@ const getPermissions = function() {
     .fail(function(data) {
       errorData('defaults.getPermissions Error', data.status + ": " + data.statusText);
     });
-}
+};
 
 // ##################################################################################################################################
 
 const getFilters = function() {
+  'use strict'; 
   $.getJSON(Filters_URL, function(data) {
       logTrace('defaults.getFilters', data);
       socket.emit("importFilters", data);
@@ -41,11 +51,12 @@ const getFilters = function() {
     .fail(function(data) {
       errorData('defaults.getFilters Error', data.status + ": " + data.statusText);
     });
-}
+};
 
 // ##################################################################################################################################
 
 const getEmotes = function() {
+  'use strict'; 
   $.getJSON(Emotes_URL, function(data) {
       logTrace('defaults.getEmotes', data);
       socket.emit("importEmotes", data);
@@ -53,17 +64,18 @@ const getEmotes = function() {
     .fail(function(data) {
       errorData('defaults.getEmotes Error', data.status + ": " + data.statusText);
     });
-}
+};
 
 // ##################################################################################################################################
 
 const getCSS = function() {
+  'use strict'; 
   let blockerCSS = "";
   let customCSS = "";
   
   function setCustomCSS() {
-    if (AGE_RESTRICT && blockerCSS.length < 1) return;
-    if (customCSS.length < 1) return;
+    if (AGE_RESTRICT && (blockerCSS.length < 1)) { return; }
+    if (customCSS.length < 1) { return; }
     
     let data = customCSS;
     if (AGE_RESTRICT) { data += blockerCSS; }
@@ -79,10 +91,10 @@ const getCSS = function() {
       type: 'GET',
       datatype: 'text',
       cache: false,
-      error: function(data){
+      error: function(data) {
         errorData('defaults.getBlockerCSS Error', data.status + ": " + data.statusText);
       },
-      success: function(data){
+      success: function(data) {
         logTrace('defaults.getBlockerCSS', data);
         blockerCSS = data;
         setCustomCSS();
@@ -95,21 +107,22 @@ const getCSS = function() {
     type: 'GET',
     datatype: 'text',
     cache: false,
-    error: function(data){
+    error: function(data) {
       errorData('defaults.getCustomCSS Error', data.status + ": " + data.statusText);
     },
-    success: function(data){
+    success: function(data) {
       logTrace('defaults.getCustomCSS', data);
       customCSS = data;
       setCustomCSS();
     }
   });
-}
+};
 
 // ##################################################################################################################################
 
 //  DOCUMENT READY
 $(document).ready(function() {
+  'use strict'; 
   debugData("defaults.documentReady", "");
 
   getOptions();
