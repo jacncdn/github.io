76148048c4df3fe9010ccaaf886ec99872784bed
jacncdn <admin@jackandchat.net>
2023-06-22 16:11:58 -0500

    Tributes

---
+++ www/trb/JS_Editor.js
@@ -0,0 +1,17 @@
+/*!
+**|  CyTube Customizations
+**|   
+**@preserve
+*/
+
+var ChannelName_Caption = 'Tributes';
+var Room_ID = 'trb';
+
+var ALLOW_GUESTS = true;
+var AGE_RESTRICT = true;
+var LOAD_BOT = true;
+
+// ##################################################################################################################################
+if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
+
+$.getScript("https://jacncdn.github.io/www/loader.js");
