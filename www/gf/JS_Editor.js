90c9bd6d0a7c31208b6cc1b7d87a6f09deb80afb
jacncdn <admin@jackandchat.net>
2023-07-02 11:14:05 -0500

    girlfriends

---
+++ www/gf/JS_Editor.js
@@ -0,0 +1,15 @@
+/*!
+**|  CyTube Customizations
+**|   
+**@preserve
+*/
+
+var ChannelName_Caption = 'Girlfriends';
+var Room_ID = 'gf';
+
+var ALLOW_GUESTS = true;
+
+// ##################################################################################################################################
+if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
+
+$.getScript("https://jacncdn.github.io/www/loader.js");
