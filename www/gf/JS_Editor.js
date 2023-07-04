6a0646f8b5e724a8c836b3a957b8464c833b2499
jacncdn <126722094+jacncdn@users.noreply.github.com>
2023-07-04 14:44:59 -0500

    Update JS_Editor.js

--- www/gf/JS_Editor.js
+++ www/gf/JS_Editor.js
@@ -8,6 +8,8 @@ var ChannelName_Caption = 'Girlfriends';
 var Room_ID = 'gf';
 
 var ALLOW_GUESTS = true;
+var LOAD_BOT = true;
+var BOT_NICK = "LarryFlynt"; 
 
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
