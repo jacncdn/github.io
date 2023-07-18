9a9182431268b7652900137b6d1b24aee53997a2
jacncdn <126722094+jacncdn@users.noreply.github.com>
2023-07-18 14:59:03 -0500

    Update JS_Editor.js

--- www/gb/JS_Editor.js
+++ www/gb/JS_Editor.js
@@ -7,7 +7,10 @@
 var ChannelName_Caption = 'Xtreme Fun';
 var Room_ID = 'gb';
 
+var AGE_RESTRICT = true;
+var ALLOW_GUESTS = true;
 var LOAD_BOT = true;
+var BOT_NICK = "LarryFlynt"; 
 
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
