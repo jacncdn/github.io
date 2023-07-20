a43da7abbf89e4a31bcb1c2fa7bf7bf69cf5554d
jacncdn <126722094+jacncdn@users.noreply.github.com>
2023-07-20 12:07:46 -0500

    18+

--- www/cum/JS_Editor.js
+++ www/cum/JS_Editor.js
@@ -7,12 +7,12 @@
 var ChannelName_Caption = 'So Much Cum!';
 var Room_ID = 'cum';
 
+var ALLOW_GUESTS = true;
 var AGE_RESTRICT = true;
+
 var LOAD_BOT = true;
 
-var CLEAR_MSG = "Here is a list of other rooms https://s.lain.la/xZP2R ";
-
-var MOTD_MSG = `<br /><span style="color:orange">Here is a list of other rooms <a style="color:orange;font-weight:600" target=_blank" href="https://s.lain.la/xZP2R">https://s.lain.la/xZP2R</a></span>`;
+var MOTD_MSG = `<br /><span style="color:orange">This is a temporary room until CyTube XYZ comes back up.</span>`;
 
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
