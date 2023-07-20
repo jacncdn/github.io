7395f73155232c5c7b3d5026ee146a1c7e48eb62
jacncdn <126722094+jacncdn@users.noreply.github.com>
2023-07-20 11:36:35 -0500

    fd

--- www/fd/JS_Editor.js
+++ www/fd/JS_Editor.js
@@ -8,8 +8,11 @@ var ChannelName_Caption = 'Familiar Faces';
 var Room_ID = 'fd';
 
 var ALLOW_GUESTS = true;
-var LOAD_BOT = true;
-var BETA_USERS = ['lisaqtee'];
+var AGE_RESTRICT = true;
+
+var LOAD_BOT = false;
+
+var MOTD_MSG = `<br /><span style="color:orange">This is a temporary room until CyTube XYZ comes back up.</span>`;
 
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
