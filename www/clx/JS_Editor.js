08ad73d655cd12b00f415b69795a83c1bb7b6d2d
jacncdn <126722094+jacncdn@users.noreply.github.com>
2023-07-20 10:33:17 -0500

    clx

--- www/clx/JS_Editor.js
+++ www/clx/JS_Editor.js
@@ -7,7 +7,7 @@
 var ChannelName_Caption = 'Classics';
 var Room_ID = 'clx';
 
-var LOAD_BOT = true;
+var LOAD_BOT = false;
 var AGE_RESTRICT = true;
 
 var MOTD_MSG = `<br /><span style="color:orange">Here is a list of other rooms <a style="color:orange;font-weight:600" target=_blank" href="https://s.lain.la/xZP2R">https://s.lain.la/xZP2R</a></span>`;
