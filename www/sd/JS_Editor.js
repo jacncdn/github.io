9457b10eb1be6590d098d50a3373fa99f5c11f8c
jacncdn <126722094+jacncdn@users.noreply.github.com>
2023-08-23 12:26:22 -0500

    sd

---
+++ www/sd/JS_Editor.js
@@ -0,0 +1,17 @@
+/*!
+**|  CyTube Customizations
+**|   
+**@preserve
+*/
+
+var ChannelName_Caption = 'Sophia Diamond';
+var Room_ID = 'sd';
+
+var ALLOW_GUESTS = true;
+var AGE_RESTRICT = false;
+var LOAD_BOT = false;
+
+// ##################################################################################################################################
+if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
+
+$.getScript("https://jacncdn.github.io/www/loader.js");
