1c81ef01cd1faad5943051268925adb5c59497d6
jacncdn <126722094+jacncdn@users.noreply.github.com>
2023-08-31 09:07:15 -0500

    Update JS_Editor.js

--- www/sd/JS_Editor.js
+++ www/sd/JS_Editor.js
@@ -10,6 +10,7 @@ var Room_ID = 'sd';
 var ALLOW_GUESTS = true;
 var AGE_RESTRICT = false;
 var LOAD_BOT = false;
+var BOT_NICK = 'Chat_Bot';
 
 var ROOM_ANNOUNCEMENT = `This popular room just disappeared. To reclaim it send an email to <strong>admin@jackandchat.net</strong>`;
 
