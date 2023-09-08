b7740e3026af3e9435f6e37788daa36eaa623942
jacncdn <126722094+jacncdn@users.noreply.github.com>
2023-09-08 13:17:46 -0500

    AM

---
+++ www/am/JS_Editor.js
@@ -0,0 +1,20 @@
+/*!
+**|  CyTube Customizations
+**|   
+**@preserve
+*/
+
+var ChannelName_Caption = 'Amouranth';
+var Room_ID = 'am';
+
+var ALLOW_GUESTS = true;
+var AGE_RESTRICT = false;
+
+var BOT_NICK = 'Chat_Bot';
+
+// Amouranth - Kaitlyn Siragusa
+
+// ##################################################################################################################################
+if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
+
+$.getScript("https://jacncdn.github.io/www/loader.js");
