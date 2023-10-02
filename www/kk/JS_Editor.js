bdb5e4ea56e031ec89e4709a9107d62c8d6716f6
jacncdn <126722094+jacncdn@users.noreply.github.com>
2023-10-02 10:28:33 -0500

    kk

---
+++ www/kk/JS_Editor.js
@@ -0,0 +1,18 @@
+/*!
+**|  CyTube Customizations
+**|   
+**@preserve
+*/
+
+var ChannelName_Caption = 'Monroe';
+var Room_ID = 'kk';
+
+var ALLOW_GUESTS = true;
+var AGE_RESTRICT = true;
+
+// var MOTD_MSG = `<br /><span style="color:orange">This is a temporary room until CyTube XYZ comes back up.</span>`;
+
+// ##################################################################################################################################
+if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
+
+$.getScript("https://jacncdn.github.io/www/loader.js");
