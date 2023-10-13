290f046f5331166d6cfbf5957c5bc4dd47546c6d
jacncdn <126722094+jacncdn@users.noreply.github.com>
2023-10-13 14:52:42 -0500

    kk

--- www/kk/JS_Editor.js
+++ www/kk/JS_Editor.js
@@ -9,9 +9,6 @@ var Room_ID = 'kk';
 
 var ALLOW_GUESTS = true;
 var AGE_RESTRICT = true;
-var MOTD_BTNS = false;
-
-// var MOTD_MSG = `<br /><span style="color:orange">This is a temporary room until CyTube XYZ comes back up.</span>`;
 
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
