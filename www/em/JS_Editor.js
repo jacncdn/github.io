5b32d32d0f363c8053ba1eeeedaabbfe56c38d8f
jacncdn <126722094+jacncdn@users.noreply.github.com>
2023-10-26 11:37:27 -0500

    em

--- www/em/JS_Editor.js
+++ www/em/JS_Editor.js
@@ -5,10 +5,11 @@
 */
 
 var ChannelName_Caption = 'Emily Monks';
-var Room_ID = 'em';
+var Room_ID = 'clone';
 
 var ALLOW_GUESTS = true;
 var AGE_RESTRICT = false;
+
 
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
