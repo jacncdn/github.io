e8e76799940e370db0b14f8b92116ac419bfddd3
jacncdn <126722094+jacncdn@users.noreply.github.com>
2023-10-29 20:34:51 -0500

    Update JS_Editor.js

--- www/sb/JS_Editor.js
+++ www/sb/JS_Editor.js
@@ -11,8 +11,6 @@ var ALLOW_GUESTS = true;
 var AGE_RESTRICT = true;
 var MOTD_BTNS = false;
 
-var MOTD_MSG = `<br /><span style="color:orange">Looking for room moderators. <a href="mailto:admin@jackandchat.net">Apply here.</a></span>`;
-
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
 
