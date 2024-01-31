431af5e594475586497ed003d2d2f381943edced
jacncdn <126722094+jacncdn@users.noreply.github.com>
2024-01-30 20:02:58 -0600

    Update JS_Editor.js

--- www/fd/JS_Editor.js
+++ www/fd/JS_Editor.js
@@ -4,16 +4,13 @@
 **@preserve
 */
 
-var ChannelName_Caption = 'Familiar Faces';
+var ChannelName_Caption = 'Kute Faces';
 var Room_ID = 'fd';
 
 var ALLOW_GUESTS = true;
 var AGE_RESTRICT = true;
 
-
 var BOT_NICK = "LarryFlynt";
-
-// var MOTD_MSG = `<br /><span style="color:orange">This is a temporary room until CyTube XYZ comes back up.</span>`;
 
 // ##################################################################################################################################
 if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
